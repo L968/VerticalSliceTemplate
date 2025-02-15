9478a7d9c67088650e432eff2357444aa62f28ba
Lucas Constantino <lucas.c968@gmail.com>
2025-02-15 13:40:53 -0300

    Adding architecture tests

---
+++ VerticalSliceTemplate.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace VerticalSliceTemplate.ArchitectureTests.Abstractions;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly ApplicationAssembly = typeof(Application.DependencyInjection).Assembly;
+}
